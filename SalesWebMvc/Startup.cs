8599773bf3c54b2368f9702966b9e60bcfdca773
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-27 22:58:01 -0300

    Implementando SellerService

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -36,6 +37,7 @@ namespace SalesWebMvc
                         builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
