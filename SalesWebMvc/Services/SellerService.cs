89007cee6520ca1e3415aa05bb384c95276782de
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-28 21:21:39 -0300

    Implementando detalhes de vendedores e o eager loading (objetos relacionados). Vendedor -> Departamento

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -27,7 +28,9 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller
+                .Include(obj => obj.Department)
+                .FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
